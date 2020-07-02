4794a7af998077a1e88ce3de69c1f60bcfa687fc
dziq45 <pidzikowski@softjourn.com>
2020-07-02 16:27:55 +0200

    Semicolons

--- server/index.js
+++ server/index.js
@@ -1,15 +1,15 @@
 const express = require('express');
-const {config} = require('./config')
-const {mongo} = require('./mongo')
+const {config} = require('./config');
+const {mongo} = require('./mongo');
 const app = express();
  
 mongo.connect()
-.then(() => {
-  app.listen(config.db.PORT, () =>
-    console.log(`Task-app listening on port ${config.db.PORT}!`),
-  );
-})
-.catch(err=>{
-  console.log('Database connection error')
-  console.log(err)
-});
+  .then(() => {
+    app.listen(config.db.PORT, () =>
+      console.log(`Task-app listening on port ${config.db.PORT}!`),
+    );
+  })
+  .catch(err=>{
+    console.log('Database connection error');
+    console.log(err);
+  });
