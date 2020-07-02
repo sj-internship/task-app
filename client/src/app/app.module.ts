c1c14f9f4cae5b8ae1e75dd95cdc0efaf12818e6
dziq45 <pidzikowski@softjourn.com>
2020-07-02 11:08:45 +0200

    Push test

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EntryComponent } from './components/page/entry/entry.component';
+import { ListComponent } from './components/page/list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EntryComponent,
+    ListComponent,
   ],
   imports: [
     BrowserModule,
