f34b1e357c1fad9cd05fec4dff4d61bf86b3ff8a
dziq45 <pidzikowski@softjourn.com>
2020-07-10 15:06:44 +0200

    temporary

--- client/src/app/models/user.ts
+++ client/src/app/models/user.ts
@@ -1,8 +1,9 @@
 export interface UserModel {
     userName: string;
+    password:string;
     token: string;
 }
 export interface UserCredentialsModel{
-    name: string,
-    password:string
+    name: string;
+    password:string;
 }
