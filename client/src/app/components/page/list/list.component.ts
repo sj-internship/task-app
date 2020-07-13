aa523bcbd8d57bf3efa958855b66baf418151fa9
dziq45 <pidzikowski@softjourn.com>
2020-07-13 09:20:12 +0200

    Created preloader with @angular/material

--- client/src/app/components/page/list/list.component.ts
+++ client/src/app/components/page/list/list.component.ts
@@ -1,36 +1,37 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import {TaskService} from '../../../services/task.service';
-import {TaskModel} from '../../../models/task';
+import { TaskService } from '../../../services/task.service';
+import { TaskModel } from '../../../models/task';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { Select2OptionData } from 'ng2-select2';
+import { LoaderService } from '../../../services/loader.service'
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent implements OnInit, OnDestroy {
-  public tasks:TaskModel[];
-  public filteredTasks:TaskModel[];
+  public tasks: TaskModel[];
+  public filteredTasks: TaskModel[];
   public selectOptions;
   private ngUnsubscribe = new Subject<void>();
   public allUniqueTags: Array<Select2OptionData> = [];
-  constructor(private taskService: TaskService) { }
+  constructor(
+    private taskService: TaskService
+  ) { }
 
   public ngOnInit() {
-
-    
     this.getAllTasks();
     this.getTags();
     this.initializeSelectOptions();
   }
-  public getAllTasks(){
-    this.taskService.getAllTasks().pipe(takeUntil(this.ngUnsubscribe)).subscribe(tasks=>{
+  public getAllTasks() {
+    this.taskService.getAllTasks().pipe(takeUntil(this.ngUnsubscribe)).subscribe(tasks => {
       this.tasks = tasks;
-      this.filteredTasks=tasks;
+      this.filteredTasks = tasks;
     });
   }
-  public ngOnDestroy(){
+  public ngOnDestroy() {
     this.ngUnsubscribe.next();
     this.ngUnsubscribe.complete();
   }
@@ -38,7 +39,8 @@ export class ListComponent implements OnInit, OnDestroy {
     return this.taskService.getTags().pipe(takeUntil(this.ngUnsubscribe)).subscribe(
       tags => {
         this.prepareTagsSelect(tags)
-      }
+      },
+      error=>{},
     );
   }
   private prepareTagsSelect(tags) {
@@ -46,27 +48,27 @@ export class ListComponent implements OnInit, OnDestroy {
       this.allUniqueTags = [...this.allUniqueTags, { id: index, text: item }];
     });
   }
-  public selectChanged(event){
-    if(event.data.length >0){
+  public selectChanged(event) {
+    if (event.data.length > 0) {
       this.filteredTasks = this.tasks.filter(task => {
         let flag = false;
         event.data.forEach(item => {
-          if(task.tags.includes(item.text)){
+          if (task.tags.includes(item.text)) {
             flag = true;
           }
         });
         return flag;
       })
     }
-    else{
+    else {
       this.filteredTasks = this.tasks;
     }
   }
-  private initializeSelectOptions(){
+  private initializeSelectOptions() {
     this.selectOptions = {
-      multiple:true, 
-      allowClear:true,
-      placeholder:'Choose a tag'
+      multiple: true,
+      allowClear: true,
+      placeholder: 'Choose a tag'
     }
   }
 }
